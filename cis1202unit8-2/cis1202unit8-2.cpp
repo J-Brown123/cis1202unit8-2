e580c7efa343aa217bbfd8a3189e1e7243d84b24
Jordan Brown <jordan.brown@sinclair.edu>
2022-10-18 15:32:43 -0400

    Initial main function

--- cis1202unit8-2/cis1202unit8-2.cpp
+++ cis1202unit8-2/cis1202unit8-2.cpp
@@ -3,6 +3,7 @@
 // Oct. 18, 2022
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -11,9 +12,8 @@ char character(char , int);
 
 int main()
 {
-	bool again = true;
-	char letter;
-	int num;
+	char letter, newChar;
+	int num, again = 1;
 	while (again)
 	{
 		try
@@ -21,7 +21,24 @@ int main()
 			cout << "Input a beginning letter.\n";
 			cin >> letter;
 			cout << "How much do you want to offset?\n";
-
+			cin >> num;
+			newChar = character(letter, num);
+		}
+		catch (string invalidCharacterException)
+		{
+			cout << invalidCharacterException;
+		}
+		catch (char invalidRangeException)
+		{
+			cout << invalidRangeException;
+		}
+		cout << "The new character is " << newChar << endl;
+		cout << "Do you want to do another?\n1 - Yes\n2 - No\n";
+		cin >> again;
+		while (again < 0 || again > 1)
+		{
+			cout << "Invalid response.\n1 - Yes\n2 - No\n";
+			cin >> again;
 		}
 	}
 	return 0;
@@ -30,9 +47,9 @@ int main()
 char character(char start, int offset)
 {
 	char newChar;
-	string invalidCharacterException, invalidRangeException;
+	string invalidCharacterException;
+	char invalidRangeException[] = "ERROR: Offset is out of range.\n";
 	invalidCharacterException = "ERROR: Please input an uppercase or lowercase letter.\n";
-	invalidRangeException = "ERROR: Offset is out of range.\n";
 	if (!isupper(start) && !islower(start))
 		throw invalidCharacterException;
 	newChar = start + offset;
